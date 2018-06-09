f12a5251c7c24e83d2616bd808be0c79e63995b0
Sergiy Tkach <bees@meta.ua>
2018-06-10 00:11:33 +0300

    Добавл. лава.

--- resources/images/tiles/tiles.tsx
+++ resources/images/tiles/tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="3" columns="0">
+<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="floor">
   <properties>
@@ -21,5 +21,12 @@
    <property name="transparent" type="bool" value="false"/>
   </properties>
   <image width="32" height="32" source="wall.png"/>
+ </tile>
+ <tile id="3" type="floor">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+   <property name="transparent" type="bool" value="true"/>
+  </properties>
+  <image width="32" height="32" source="lava.png"/>
  </tile>
 </tileset>
